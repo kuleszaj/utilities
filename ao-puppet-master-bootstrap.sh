f34524c494ae6df0a28b963fd7e51ca1dfb1bdce
Justin Kulesza <kulesza.j@gmail.com>
2011-11-11 10:30:17 -0500

    Add iptables rules, ntpdate setup

--- ao-puppet-master-bootstrap.sh
+++ ao-puppet-master-bootstrap.sh
@@ -1,10 +1,14 @@
 set -ex
 
-apt-get install ruby rubygems ||
-yum install -y ruby rubygems ||
-pkgin install ruby rubygems ||
+# Get ruby, rubygems, and npdtae
+apt-get install ruby rubygems ntpdate ||
+yum install -y ruby rubygems nptdate ||
 false
 
+# Run ntpdate to ensure the system date is correct
+ntpdate pool.ntpd.org
+
+# Update rubygems, and pull down facter and then puppet
 gem update --system
 gem install facter
 gem install puppet
@@ -16,13 +20,29 @@ cat >/etc/puppet/puppet.conf <<EOF
   rundir = /var/run/puppet
   ssldir = \$vardir/ssl
   vardir = /var/lib/puppet
-
   pluginsync = true
-
-  server = "puppet-master-centos.atomicobject.localnet"
-
-  environment = "production"
 EOF
 
-echo "You should ensure that the hostname is properly set, in both /etc/hosts and in your network config."
+# Add firewall rule to allow puppet agent connections
+iptables -I INPUT 1 -p tcp --dport 8140 -j ACCEPT
+
+# Save firewall configure
+OS=`facter osfamily`
+OS=`echo $OS | tr [:upper:] [:lower:]`
+case $OS in
+  "redhat")
+    echo "Detected Redhat Family"
+    iptables-save > /etc/sysconfig/iptables
+    ;;
+  "debian")
+    echo "Detected Debian Family"
+    iptables-save > /etc/iptables.rules
+    ;;
+  *)
+    echo "Uknown OS"
+    ;;
+esac
+
+echo "You should ensure that your intended hostname is properly set, in both /etc/hosts and in your network config."
+echo "Currently, the hostname is: $(facter fqdn)"
 echo "After this, run: puppet master --mkuser"
