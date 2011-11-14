29832e17605db48609198508bb6d6dd023fd9b72
Justin Kulesza <kulesza.j@gmail.com>
2011-11-14 18:12:59 -0500

    Try rvm install

--- ao-puppet-master-bootstrap.sh
+++ ao-puppet-master-bootstrap.sh
@@ -1,55 +1,53 @@
-wget http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
-rpm --import http://ftp.osuosl.org/pub/fedora-epel/RPM-GPG-KEY-EPEL-5
-rpm -K epel-release-5-4.noarch.rpm
-rpm -i epel-release-5-4.noarch.rpm
+# Get Rubyforge, install
 wget http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 rpm -K rpmforge-release-0.5.2-2.el5.rf.*.rpm
 rpm -i rpmforge-release-0.5.2-2.el5.rf.*.rpm
 
 # Get ruby, rubygems, and npdtae
-apt-get install ruby rubygems ntpdate ||
-yum install -y ruby rubygems ntpdate ||
-false
+yum install -y git subversion readline readline-devel zlib zlib-devel openssl openssl-devel autoconf  ntp
 
 # Run ntpdate to ensure the system date is correct
-ntpdate pool.ntp.org
+#ntpdate pool.ntp.org
+
+# Install RVM
+bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
 # Update rubygems, and pull down facter and then puppet
-gem update --system
-gem install facter
-gem install puppet
+#gem update --system
+#gem install facter
+#gem install puppet
 
 mkdir -p /etc/puppet /var/lib /var/log /var/run
-cat >/etc/puppet/puppet.conf <<EOF
-[main]
-  logdir = /var/log/puppet
-  rundir = /var/run/puppet
-  ssldir = \$vardir/ssl
-  vardir = /var/lib/puppet
-  pluginsync = true
-EOF
-
-# Add firewall rule to allow puppet agent connections
-iptables -I INPUT 1 -p tcp --dport 8140 -j ACCEPT
-
-# Save firewall configure
-OS=`facter osfamily`
-OS=`echo $OS | tr [:upper:] [:lower:]`
-case $OS in
-  "redhat")
-    echo "Detected Redhat Family"
-    iptables-save > /etc/sysconfig/iptables
-    ;;
-  "debian")
-    echo "Detected Debian Family"
-    iptables-save > /etc/iptables.rules
-    ;;
-  *)
-    echo "Uknown OS"
-    ;;
-esac
-
-echo "You should ensure that your intended hostname is properly set, in both /etc/hosts and in your network config."
-echo "Currently, the hostname is: $(facter fqdn)"
-echo "After this, run: puppet master --mkuser"
+#cat >/etc/puppet/puppet.conf <<EOF
+#[main]
+#  logdir = /var/log/puppet
+#  rundir = /var/run/puppet
+#  ssldir = \$vardir/ssl
+#  vardir = /var/lib/puppet
+#  pluginsync = true
+#EOF
+#
+## Add firewall rule to allow puppet agent connections
+#iptables -I INPUT 1 -p tcp --dport 8140 -j ACCEPT
+#
+## Save firewall configure
+#OS=`facter osfamily`
+#OS=`echo $OS | tr [:upper:] [:lower:]`
+#case $OS in
+#  "redhat")
+#    echo "Detected Redhat Family"
+#    iptables-save > /etc/sysconfig/iptables
+#    ;;
+#  "debian")
+#    echo "Detected Debian Family"
+#    iptables-save > /etc/iptables.rules
+#    ;;
+#  *)
+#    echo "Uknown OS"
+#    ;;
+#esac
+#
+#echo "You should ensure that your intended hostname is properly set, in both /etc/hosts and in your network config."
+#echo "Currently, the hostname is: $(facter fqdn)"
+#echo "After this, run: puppet master --mkuser"
