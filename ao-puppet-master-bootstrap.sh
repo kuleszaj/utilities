a1afd65875e5459be372d28953320dc376f00b87
Justin Kulesza <kulesza.j@gmail.com>
2011-11-14 18:25:19 -0500

    update

--- ao-puppet-master-bootstrap.sh
+++ ao-puppet-master-bootstrap.sh
@@ -13,41 +13,47 @@ ntpdate pool.ntp.org
 # Install RVM
 bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
+bash <<END
+
+rvm install 1.8.7
+
 # Update rubygems, and pull down facter and then puppet
-#gem update --system
-#gem install facter
-#gem install puppet
+gem update --system
+gem install facter
+gem install puppet
 
 mkdir -p /etc/puppet /var/lib /var/log /var/run
-#cat >/etc/puppet/puppet.conf <<EOF
-#[main]
-#  logdir = /var/log/puppet
-#  rundir = /var/run/puppet
-#  ssldir = \$vardir/ssl
-#  vardir = /var/lib/puppet
-#  pluginsync = true
-#EOF
-#
-## Add firewall rule to allow puppet agent connections
-#iptables -I INPUT 1 -p tcp --dport 8140 -j ACCEPT
-#
-## Save firewall configure
-#OS=`facter osfamily`
-#OS=`echo $OS | tr [:upper:] [:lower:]`
-#case $OS in
-#  "redhat")
-#    echo "Detected Redhat Family"
-#    iptables-save > /etc/sysconfig/iptables
-#    ;;
-#  "debian")
-#    echo "Detected Debian Family"
-#    iptables-save > /etc/iptables.rules
-#    ;;
-#  *)
-#    echo "Uknown OS"
-#    ;;
-#esac
-#
-#echo "You should ensure that your intended hostname is properly set, in both /etc/hosts and in your network config."
-#echo "Currently, the hostname is: $(facter fqdn)"
-#echo "After this, run: puppet master --mkuser"
+cat >/etc/puppet/puppet.conf <<EOF
+[main]
+  logdir = /var/log/puppet
+  rundir = /var/run/puppet
+  ssldir = \$vardir/ssl
+  vardir = /var/lib/puppet
+  pluginsync = true
+EOF
+
+# Add firewall rule to allow puppet agent connections
+iptables -I INPUT 1 -p tcp --dport 8140 -j ACCEPT
+
+# Save firewall configure
+OS=`facter osfamily`
+OS=`echo $OS | tr [:upper:] [:lower:]`
+case $OS in
+  "redhat")
+    echo "Detected Redhat Family"
+    iptables-save > /etc/sysconfig/iptables
+    ;;
+  "debian")
+    echo "Detected Debian Family"
+    iptables-save > /etc/iptables.rules
+    ;;
+  *)
+    echo "Uknown OS"
+    ;;
+esac
+
+echo "You should ensure that your intended hostname is properly set, in both /etc/hosts and in your network config."
+echo "Currently, the hostname is: $(facter fqdn)"
+echo "After this, run: puppet master --mkuser"
+
+END
