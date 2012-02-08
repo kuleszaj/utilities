a6a2120fac0e862298eb38d5adf3e47d6112ba76
Justin Kulesza <kulesza.j@gmail.com>
2012-02-08 09:24:07 -0500

    Production boostrap script

---
+++ prd-puppet-boostrap.sh
@@ -0,0 +1,54 @@
+# Get Rubyforge, install
+wget http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el5.rf.x86_64.rpm
+sudo rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+sudo rpm -K rpmforge-release-0.5.2-2.el5.rf.*.rpm
+sudo rpm -i rpmforge-release-0.5.2-2.el5.rf.*.rpm
+
+# Get ruby, rubygems, and npdtae
+sudo yum install -y glibc gcc-c++ patch make bzip2 autoconf automake libtool bison git subversion readline readline-devel zlib zlib-devel openssl openssl-devel libyaml-devel libffi-devel  ntp
+
+# Run ntpdate to ensure the system date is correct
+sudo ntpdate pool.ntp.org
+
+
+echo "insecure" > ~/.curlrc
+# Install RVM
+sudo bash -s stable < <(curl -k -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+
+source /etc/profile.d/rvm.sh
+
+rvmsudo rvm install 1.8.7
+
+sudo su - -c 'rvm 1.8.7 --default'
+
+source /etc/profile.d/rvm.sh
+rvm 1.8.7 --default
+
+# Update rubygems, and pull down facter and then puppet
+rvmsudo gem update --system
+rvmsudo gem install facter --no-ri --no-rdoc
+rvmsudo gem install puppet --no-ri --no-rdoc
+
+sudo mkdir -p /etc/puppet /var/lib /var/log /var/run
+echo "nameserver 10.0.132.5" > ~/resolv.conf
+sudo mv ~/resolv.conf /etc/resolv.conf
+cat >~/puppet.conf <<EOF
+[main]
+  logdir = /var/log/puppet
+  rundir = /var/run/puppet
+  ssldir = \$vardir/ssl
+  vardir = /var/lib/puppet
+
+  pluginsync = true
+
+  server = "ashnazg.sme.loc"
+
+  environment = "prd"
+EOF
+sudo mv ~/puppet.conf /etc/puppet/puppet.conf
+
+echo "On the puppet master, run: puppet cert -s $(facter fqdn)" >&2
+echo "When done, press <ENTER>"
+rvmsudo puppet agent --no-daemonize --onetime --no-splay --verbose --waitforcert 120
+read
+rvmsudo puppet agent
